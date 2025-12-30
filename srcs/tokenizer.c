b060646bed31b180124b0c22dd1fc731a89b198e
LnK-Laptop <aandreo.4202@gmail.com>
2025-12-30 01:09:40 +0100

    reorganized tokenizer's logic by adding flags to handle quotes and splitting functions so its more readable

--- srcs/tokenizer.c
+++ srcs/tokenizer.c
@@ -6,69 +6,115 @@
 /*   By: aandreo <aandreo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 18:46:01 by aandreo           #+#    #+#             */
-/*   Updated: 2025/12/28 02:06:39 by aandreo          ###   ########.fr       */
+/*   Updated: 2025/12/30 01:08:25 by aandreo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Minishell.h"
 
-//split input into tokens and prints them (atm)
-//TODO need to add -> create linked list and fill tokens in it
-//TODO			   -> handle special tokens | < > >>
-//TODO			   -> handle quotes ('' && "")
-//TODO			   -> handle escape characters
-void tokenizer(char *input)
+//	split input into tokens and return linked list of tokens
+//	this function return the token type base on value and length
+static t_token_type get_type(char *value, int has_quotes)
 {
-	int len;
+	if (has_quotes) // -> if quote = always a word
+		return (TOKEN_WORD);
+	if (ft_strlen(value) == 1 && value[0] == '|')
+		return (TOKEN_PIPE);
+	if (ft_strlen(value) == 1 && value[0] == '<')
+		return (TOKEN_REDIRECT_IN);
+	if (ft_strlen(value) == 2 && value[0] == '<' && value[1] == '<')
+		return (TOKEN_HEREDOC);
+	if (ft_strlen(value) == 1 && value[0] == '>')
+		return (TOKEN_REDIRECT_OUT);
+	if (ft_strlen(value) == 2 && value[0] == '>' && value[1] == '>')
+		return (TOKEN_APPEND);
+	return (TOKEN_WORD);
+}
+
+//	return index of end of word to tokenize "token per token"
+//	handled quotes with flags -> has_quotes / in_single / in_double
+static int find_end_of_word(char *word, int *has_quotes)
+{
 	int i;
-	int j;
+	int in_single;
+	int in_double;
 
 	i = 0;
+	in_single = 0;
+	in_double = 0;
+	*has_quotes = 0;
+	while(word[i])
+	{
+		if(word[i] == '\'' && !in_double)
+		{
+			in_single = !in_single;
+			*has_quotes = 1;
+		}
+		else if(word[i] == '\"' && !in_single)
+		{
+			in_double = !in_double;
+			*has_quotes = 1;
+		}
+		else if(!in_single && !in_double
+			&& (is_space(word[i]) || word[i] == '|' || word[i] == '<' || word[i] == '>'))
+			break;
+		i++;
+	}
+	if(in_double || in_single)
+		return (-1);
+	return (i);
+}
+
+static int get_token_len(char *input, int *has_quotes)
+{
+	*has_quotes = 0;
+
+	// check if operator
+	if (input[0] == '|')
+		return (1);
+	if (input[0] == '<')
+	{
+		if (input[1] == '<')
+			return (2);
+		return (1);
+	}
+	if (input[0] == '>')
+	{
+		if (input[1] == '>')
+			return (2);
+		return (1);
+	}
+	// if not == word -> need to find end of word
+	return (find_end_of_word(input, has_quotes));
+}
+
+t_token *tokenizer(char *input)
+{
+	int		len;
+	int		i = 0;
+	int		has_quotes;
+	char	*value;
+	t_token	*tokens = NULL;
+	t_token	*new;
+
 	while(input[i])
 	{
 		while(is_space(input[i]))
 			i++;
 		if(!input[i])
 			break;
-		if (input[i] == '|')
-		{
-			printf("Token [PIPE]\n");
-			i++;
-			continue;
-		}
-		if (input[i] == '>' && input[i + 1] == '>')
-		{
-			printf("Token [APPEND >>]\n");
-			i += 2;
-			continue;
-		}
-		if (input[i] == '>')
-		{
-			printf("Token [REDIRECT_OUT >]\n");
-			i++;
-			continue;
-		}
-		if (input[i] == '<' && input[i + 1] == '<')
-		{
-			printf("Token [HEREDOC <<]\n");
-			i += 2;
-			continue;
-		}
-		if (input[i] == '<')
-		{
-			printf("Token [REDIRECT_IN <]\n");
-			i++;
-			continue;
-		}
-		len = find_end_of_word(&input[i]);
-		j = 0;
-		while(j < len)
-		{
-			printf("%c", input[i + j]);
-			j++;
-		}
-		printf(" ");
+		len = get_token_len(&input[i], &has_quotes);
+		if (len < 0)
+			return (printf("Unclosed quote\n"), free_tokens(tokens), NULL);
+		value = ft_substr(input, i, len);
+		if (!value)
+			return (free_tokens(tokens), NULL);
+		new = create_token(value, get_type(value, has_quotes));
+		if (!new)
+			return (free(value), free_tokens(tokens), NULL);
+		token_addback(&tokens, new);
 		i += len;
 	}
-	printf("\n");
+	return (tokens);
 }
+
